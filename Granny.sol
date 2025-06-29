ef6a7c75969a72ef2dae41ea2b0d9d461a1c603b
vadym-udaltsov <vadym.udaltsov@gmail.com>
2025-06-29 09:31:04 +0200

    finished contract

--- Granny.sol
+++ Granny.sol
@@ -42,7 +42,43 @@ contract Granny {
         );
         arrGrandchilds.push(walletAddress);
         counter++;
-        //emit NewGrandChild(walletAddress, name, birthday);
+        emit NewGrandChild(walletAddress, name, birthday);
+    }
+
+    // Хорошая практика использовать паттерн withdrawn ( требование ) вместо send ( отправка )
+    function withdraw() public {
+        address payable walletAddress = payable(msg.sender);
+        
+        require(
+            grandchilds[walletAddress].exist == true, 
+            "There is not such grandchild!"
+        );
+        require(
+            block.timestamp > grandchilds[walletAddress].birthday,
+            "Birthday hasn't arrived yet!"
+        );
+        require(
+            grandchilds[walletAddress].alreadyGotMoney == false,
+            "You have already received money!"
+        );
+
+        uint256 amount = bank / counter;
+        grandchilds[walletAddress].alreadyGotMoney = true;
+
+        (bool success, ) = walletAddress.call{value: amount}("");
+        require(success);
+
+        emit GotMoney(walletAddress);
+    }
+
+    function readGrandChildsArray(uint cursor, uint length) public view returns (address[] memory) {
+        address[] memory array = new address[](length);
+        uint counter2 = 0;
+        for (uint i = cursor; i < cursor + length; i++) {
+            array[counter2] = arrGrandchilds[i];
+            counter2++;
+        }
+         return array;
     }
 
     function balanceOf() public view returns (uint256) {
@@ -52,6 +88,8 @@ contract Granny {
     receive() external payable { 
         bank += msg.value;
     }
-    
+
+    event NewGrandChild(address indexed walletAddress, string name, uint256 birthday);
+    event GotMoney(address indexed walletAddress);
 }
 
